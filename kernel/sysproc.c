c48d471da5c2198abeabb93fca45b667b2df4adf
Zhao Zhiwei <zhiwei.zhao@garena.cn>
2022-01-19 20:17:30 +0800

    alarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -97,3 +97,30 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void)
+{
+
+  int interval;
+  if (argint(0, &interval) < 0)
+  {
+    return -1;
+  }
+  uint64 handler;
+  if (argaddr(1, &handler) < 0)
+    return -1;
+
+  struct proc *p = myproc();
+  p->on = 1;
+  p->interval = interval;
+  p->handler = handler;
+  return 0;
+}
+
+uint64 sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  p->on = 1;
+  *(p->trapframe) = p->alarm_trapframe;
+  return 0;
+}
