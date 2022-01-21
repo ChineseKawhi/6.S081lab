4a97844d0a20abf4a1fe54b8acb6c99c70152895
Zhao Zhiwei <zhiwei.zhao@garena.cn>
2022-01-21 14:47:29 +0800

    temp

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,47 +14,62 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct
+{
   struct spinlock lock;
   struct run *freelist;
 } kmem;
 
-void
-kinit()
+uint64 refcnt[(PHYSTOP - KERNBASE) / PGSIZE];
+
+void kincrref(void *pa)
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  uint64 index = (PGROUNDUP((uint64)pa) - PGROUNDUP((uint64)end)) / PGSIZE;
+  refcnt[index]++;
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  memset(refcnt, 0, sizeof(refcnt));
+  freerange(end, (void *)PHYSTOP);
+}
+
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  {
+    kincrref(p);
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  uint64 index = (PGROUNDUP((uint64)pa) - PGROUNDUP((uint64)end)) / PGSIZE;
+  if (--refcnt[index] > 0)
+    return;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -72,11 +87,15 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
+  {
     kmem.freelist = r->next;
+    uint64 index = (PGROUNDUP((uint64)r) - PGROUNDUP((uint64)end)) / PGSIZE;
+    refcnt[index] = 1;
+  }
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
 }
